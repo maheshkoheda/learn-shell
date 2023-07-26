b16c1a767fb905eb5d235a6c99816a08e62bd599
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-07-26 20:37:26 +0530

    Echo command

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,24 @@
 echo Hello world
 echo ">>>>>>> Hello world <<<<<<<<"
+# Red        31
+#Green        32
+#Yellow       33
+#Blue         34
+#Magenta      35
+#Cyan         36
+
+#Syntax
+#echo -e "\e[CODEmMESSAGE\e[0m"
+
+echo -e "\e[31mMESSAGE\e[0m"
+echo -e "\e[32mMESSAGE\e[0m"
+echo -e "\e[33mMESSAGE\e[0m"
+echo -e "\e[34mMESSAGE\e[0m"
+echo -e "\e[35mMESSAGE\e[0m"
+echo -e "\e[36mMESSAGE\e[0m"
+
+
+
+
+
 
