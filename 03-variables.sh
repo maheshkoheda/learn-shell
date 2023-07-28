2be2b0c40b5619c4046da4dd7b9bfa6a787b7382
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-07-28 13:52:06 +0530

    Variables

---
+++ 03-variables.sh
@@ -0,0 +1,14 @@
+#variables
+a=10
+echo a is $a
+
+#special variables
+# $0 - $N, $*, $#
+
+#Substitution variables
+#command substitution
+DATE = $(date)
+echo Today Date is $DATE
+#Arithmetic substitution
+ADD=$(( 2+2 ))
+echo ADD of 2+2 = $ADD
