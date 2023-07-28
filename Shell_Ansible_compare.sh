0d1212d1c89db0ac12bf951963a0aafcbb524c02
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-07-28 15:46:03 +0530

    Shell and Ansible comparision

---
+++ Shell_Ansible_compare.sh
@@ -0,0 +1,11 @@
+#Problems of shell script
+Shell is imperative. Very much manual and need efforts telling everything.
+Shell is homogenous. Not sure if it works in other OS versions.
+Shell is pain at scale.
+
+#Configuration management tools
+CFEngine -old tool
+Puppet- Was good
+Chef- Upgrade of puppet
+Ansible- Current leading tool
+
