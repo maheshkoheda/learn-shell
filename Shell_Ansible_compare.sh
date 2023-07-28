fc49f259bf864cdc0a246e6b841ca01aeca89fb8
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-07-28 16:24:54 +0530

    Shell and Ansible comparision

--- Shell_Ansible_compare.sh
+++ Shell_Ansible_compare.sh
@@ -11,4 +11,7 @@ Ansible- Current leading tool
 - It is declarative. Less pain and less manual it understands without telling in cases.
 - Heterogenous and can scale to large infrastructure.
 -Supports both Push and Pull approach. Should select based on our requirment.
+-Michael developed and opensourced. RedHat acquired Ansible.
+-Now Ansible core 2.15 version used for redhat ansible 8 is upgraded enhanced.
+
 
