c20138af99c40029dc91f2304b2c64cb6c1ff63d
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-07-28 15:04:09 +0530

    Environment variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -12,3 +12,14 @@ echo Today Date is $DATE
 #Arithmetic substitution
 ADD=$(( 2+2 ))
 echo ADD of 2+2 = $ADD
+
+#Access environment variables which are declared in OS.
+echo Username - $USER
+#Type env in terminal for all env variables
+#To declare custom environment variables to allow access to all
+export abc=100
+echo Env Var abc - $abc
+
+
+
+
